71bdc2492e68475424321f547efa9e4f68c56206
Sarah Sheppard <sarahmariesheppard@gmail.com>
2021-08-14 12:53:38 -0400

    final routes

--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,54 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Student = require("../db/models/student");
+const Test = require("../db/models/test");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const tests = await Test.findAll();
+    res.send(tests);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const test = await Test.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.send(test);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/student/:studentId", async (req, res, next) => {
+  try {
+    const test = await Test.create(req.body);
+
+    const student = await Student.findByPk (req.params.studentId);
+
+    await test.setStudent(student);
+
+    res.status(201).send(test);
+
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Test.destroy({where: {
+      id: req.params.id,
+    }});
+    res.status(204).send();
+  }
+  catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
